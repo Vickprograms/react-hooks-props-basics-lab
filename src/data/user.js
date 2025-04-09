250672eced761565081270f373c92aa2bbdf2426
Victor <victormugo08@gmail.com>
2025-04-09 19:28:36 +0300

    modified App.js

--- src/data/user.js
+++ src/data/user.js
@@ -1,11 +1,11 @@
 const user = {
-  name: "Liza",
-  city: "New York",
+  name: "Victor",
+  city: "Nairobi",
   bio: "I made this!",
   color: "firebrick",
   links: {
-    github: "https://github.com/liza",
-    linkedin: "https://www.linkedin.com/in/liza/",
+    github: "https://github.com/vickprograms",
+    linkedin: "https://www.linkedin.com/in/victor/",
   },
 };
 
